94efec5ac1122bcb5e69b9625439917298aef54a
Parker <parkerlopez17@gmail.com>
2024-07-24 13:05:21 -0700

    added more details to train.py template

---
+++ model/train.py
@@ -0,0 +1,20 @@
+# imports
+
+
+def train(**kwargs):
+
+    # Train Test Split
+
+    # Fit model
+
+    # Save model
+
+    pass
+
+def main():
+    # Accept arguments via CLI for model training. All of these will be provided when initiate the training job.
+
+    train()
+
+if __name__ == "__main__":
+    main()
